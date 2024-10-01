e2557d70072f9a81a30382a9ea9fba0d005f6d57
oshima <kook81@gmail.com>
2024-10-01 15:08:59 +0900

    Initial Commit

---
+++ calcKatsuApp/calcKatsuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  calcKatsuAppApp.swift
+//  calcKatsuApp
+//
+//  Created by sre10 on 2024/10/01.
+//
+
+import SwiftUI
+
+@main
+struct calcKatsuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
